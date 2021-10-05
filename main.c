b236e9184f3d1f7581b6d06f18ead9a115625b0b
Yuqing Wu <ywu20@stuy.edu>
2021-10-04 20:51:08 -0400

    average array

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
+#define LEN 6
 // function for printing array
-void printArr (int arr[], int len){
+void print_arr (int arr[], int len){
   printf("[");
   int i = 0;
   for(;i<len;i++){
@@ -11,8 +12,20 @@ void printArr (int arr[], int len){
   printf("]\n");
 }
 
+//returns average of values in array
+double avg_arr (int arr[], int len){
+  double sum = 0;
+  int i;
+  for(i=0; i<len;i++){
+    sum+=*arr;
+    arr++;
+  }
+  return sum/len;
+}
+
 int main(){
-  int arr[4] = {1,2,3,4};
-  printArr(arr,4);
+  int arr[LEN] = {1,2,3,4,5,6};
+  print_arr(arr,LEN);
+  printf("%lf\n",avg_arr(arr,LEN));
   return 0;
 }
