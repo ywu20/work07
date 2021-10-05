0b97403402230462bf830622091f2771e3d94dcd
Yuqing Wu <ywu20@stuy.edu>
2021-10-04 20:54:34 -0400

    copy array

--- main.c
+++ main.c
@@ -23,9 +23,21 @@ double avg_arr (int arr[], int len){
   return sum/len;
 }
 
+//copy array
+void cp_arr (int arr1[], int arr2[], int len){
+int i;
+  for(i=0;i<len;i++){
+    *arr2 = *arr1;
+    arr1++;
+    arr2++;
+  }
+}
 int main(){
   int arr[LEN] = {1,2,3,4,5,6};
   print_arr(arr,LEN);
   printf("%lf\n",avg_arr(arr,LEN));
+  int arr2[LEN] ={0,0,0,0,0,0};
+  cp_arr(arr,arr2,LEN);
+  print_arr(arr2,LEN);
   return 0;
 }
